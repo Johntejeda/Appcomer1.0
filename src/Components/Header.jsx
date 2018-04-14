702191414be2aed77e596734aafc26c6b094d33a
armandocano91 <armandocano.ac@gmail.com>
2018-04-14 14:13:33 -0500

    Setup files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div bsClass="App">
+          <h1 bsClass="App-title">It works!</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
