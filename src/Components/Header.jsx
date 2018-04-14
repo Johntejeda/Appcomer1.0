3ad1d528c6235f10db476701a67eecabd1a145a1
armandocano91 <armandocano.ac@gmail.com>
2018-04-14 16:11:26 -0500

    updated header and changed title style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,13 +5,10 @@ class Header extends Component {
   render() {
     return (
       <div class="page-header">
-   <h1>AppComer <small>Find the food you want!</small></h1>
+   <h1 class="page-title">AppComer <small>Find the food you want!</small></h1>
    </div>  
     );
   }
 }
 
 export default Header;
-
-
-
