3df401e8cba46be0b0e1ea75990c51665931b00f
Johntejeda <Johntejeda1969@gmail.com>
2018-04-17 11:16:57 -0500

    Commit search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< d3a7f6cd518736a53d92704486fd81b4c79c42d9
-
-import Map from './Map.jsx';
-
-import Search from  './Components/Search.jsx'
-import Header from './Components/Header.jsx'
-import TableList from './Components/TableList.jsx'
-
-=======
 import Search from  './Components/Search.jsx';
->>>>>>> implemented map
 import './App.css';
 import Header from './Components/Header.jsx';
 
@@ -21,67 +11,10 @@ class App extends Component {
   
   render() {
     return (
-<<<<<<< d3a7f6cd518736a53d92704486fd81b4c79c42d9
-     
-=======
           
           <Search />
->>>>>>> implemented map
     );
   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
