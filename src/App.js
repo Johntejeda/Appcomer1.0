c505460416fc0778cf641b5913eb82f0a6dbea8d
armandocano91 <armandocano.ac@gmail.com>
2018-04-16 19:15:49 -0500

    Added curly braces to app.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,33 @@
 import React, { Component } from 'react';
-
-import Map from './Map.jsx';
-
-import Search from  './Components/Search.jsx'
-import Header from './Components/Header.jsx'
-import TableList from './Components/TableList.jsx'
-
+import Header from './Components/Header.jsx';
+import TableList from './Components/TableList.jsx';
 import './App.css';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
 
 
 class App extends Component {
   constructor(props){
     super(props);
   }
-  
+
   render() {
     return (
-     
+
+      <Grid>
+        <Row>
+          <Header />
+        </Row>
+        <Row>
+          <Col md={8}>
+          <Map />
+          </Col>
+          <Col md={4}>
+            <TableList />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
