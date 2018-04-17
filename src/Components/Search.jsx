d3a7f6cd518736a53d92704486fd81b4c79c42d9
Johntejeda <Johntejeda1969@gmail.com>
2018-04-16 19:47:08 -0500

    modified Search component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import {form,FormGroup,FormControl} from "react-bootstrap";
+import React, {Component} from 'react';
+import axios from 'axios';
+ 
 
+const {API_KEY} = 'AIzaSyBD2x3uoBXe8vWtIaBkvRvlrD9msa-XhnM';
+const API_URL = 'https://maps.googleapis.com/maps/api/';
 
 class Search extends Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  getInfo = () => {
+    axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`).then(({data}) => {
+      this.setState({
+        results: data.data
+      })
+    })
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+         if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      }
+    })
+  }
+
   render() {
     return (
-      <form className="search">
-      <FormGroup>
-
-    <FormControl type="text" placeholder="Search Restaurant" />
-
-      </FormGroup>
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.query}</p>
       </form>
-
-
     )
   }
-};
+}
 
-export default Search;
+export default Search
